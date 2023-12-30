7bcdc3c788d124469259a986224e2124645b2282
pocket0yuka <yuka310u00@gmail.com>
2023-12-30 11:39:46 +0900

    feat: ログイン、ログアウト機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root "homes#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
