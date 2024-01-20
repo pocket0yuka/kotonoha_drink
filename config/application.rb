6aadf60310c2caf55de3b4918e68a1b0b92d0312
pocket0yuka <yuka310u00@gmail.com>
2024-01-20 16:26:51 +0900

    refactor: 画面遷移の修正、変更、パスの修正

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module KotonohaDrink
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :ja
     config.i18n.load_path+=Dir[Rails.root.join('config','locales','**','*.yml').to_s]
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,16 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
-  #Userがログインしている場合のアクセス
+  #ログイン後にリダイレクトするパスを設定
   devise_scope :user do
+    # ログアウトパス
     get '/users/sign_out' => 'devise/sessions#destroy'
-    resource :profile, only: %i[show edit update]
-    resources :bookmarks, only: %i[index create destroy show]
   end
 
   root "homes#index"
-  get '/generatedresults', to: 'generatedresults#show'
+  # ログイン後、drink_menusにリダイレクト(application_controller参照)
   get '/drink_menus', to: 'drink_menus#index'
+  get '/generatedresults', to: 'generatedresults#show'
+  resource :profile, only: %i[show edit update]
+  resources :bookmarks, only: %i[index create destroy show]
 end
