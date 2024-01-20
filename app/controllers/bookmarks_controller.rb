a78ab757a14dbac1e61cedbb589ec9a77a7fc25b
pocket0yuka <yuka310u00@gmail.com>
2024-01-20 23:15:26 +0900

    fix: 更新時にパラメータ関連のエラーが発生した為修正

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,7 +1,7 @@
 class BookmarksController < ApplicationController
   #ユーザーが(createとnewのみ)ログインしていることを確認
   before_action :authenticate_user!
-  before_action :set_bookmark, only: %i[show edit destroy]
+  before_action :set_bookmark, only: %i[show edit update destroy]
 
   # ユーザーの生成結果を一覧表示(並び替え、検索あり、モデル参照)
   def index
@@ -14,13 +14,21 @@ class BookmarksController < ApplicationController
   def edit
   end
 
+  def update
+    if @bookmark.update(bookmark_params)
+      redirect_to bookmark_path, notice: 'ブックマークを更新しました。'
+    else
+      render :edit, alert: 'ブックマークの更新に失敗しました。'
+    end
+  end
+
   #フォームから送信されたデータを使用して新しいレコードを作成
   def create
     @bookmark = current_user.bookmarks.new(bookmark_params)
     # フォームから保存したものはis_originalとする。パラメータが含まれていない場合、APIからのレスポンスとして扱う
     @bookmark.is_original = bookmark_params[:is_original].present? ? bookmark_params[:is_original] : false
     if @bookmark.save
-      redirect_to bookmarks_path, notice: 'ブックマークを保存しました。'
+      redirect_to bookmarks_path
     else
       render :new, alert: 'ブックマークの保存に失敗しました。'
     end
