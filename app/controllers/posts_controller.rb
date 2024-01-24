624097de5879f373558722f2e6e04d96953a0474
pocket0yuka <yuka310u00@gmail.com>
2024-01-24 20:42:35 +0900

    refactor:N+1問題に関する修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[show edit update destroy]
 
   def index
-    @posts = Post.where(visibility: Post.visibilities[:公開]).order(created_at: :desc)
+    @posts = Post.includes(:user).where(visibility: Post.visibilities[:公開]).order(created_at: :desc)
   end
 
   def show
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,4 @@
 class Tag < ApplicationRecord
   has_many :posttags, dependent: :destroy
+
 end
