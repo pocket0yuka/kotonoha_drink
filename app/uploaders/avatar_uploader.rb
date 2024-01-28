d6ea46df1a84fe81e19e4a6982f7aeb54b2126e3
pocket0yuka <yuka310u00@gmail.com>
2024-01-28 15:43:20 +0900

    style:レイアウト調整

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -15,7 +15,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url(*args)
-    "runtekun_1.png"
+    "kotonoha_drink logo.png"
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #
@@ -28,8 +28,11 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
-  process resize_to_limit: [100, 100]
-  # Create different versions of your uploaded files:
+
+  #画像を100x100pxにリサイズ
+  process resize_to_limit: [250, 250]
+
+  #サムネイルバージョン用で50x50pxにリサイズ
   version :thumb do
     process resize_to_fit: [50, 50]
   end
