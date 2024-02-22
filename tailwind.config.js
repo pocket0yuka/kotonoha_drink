503d1f7badcc4e21a475dcfd3132a0524fcbbc8d
pocket0yuka <yuka310u00@gmail.com>
2024-02-22 20:07:12 +0900

    feat:アニメーション付きフラッシュメッセージ追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,19 @@ module.exports = {
     './app/javascript/**/*.js'
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        flashFade: {
+          "0%": { transform: "translateX(180px)", opacity: 0 },
+          "20%": { transform: "translateX(0)", opacity: 1 },
+          "80%": { transform: "translateX(0)", opacity: 1 },
+          "100%": { transform: "translateX(180px)", opacity: 0 },
+        },
+      },
+      animation: {
+        flash: "flashFade 7.0s forwards",
+      },
+    },
     fontFamily: {
       pops: ["Poppins","sans-serif"],
       maru: ["Zen Maru Gothic", "serif"],
