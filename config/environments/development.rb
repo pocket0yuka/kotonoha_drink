ece1b158304e907b69be1fa608950bd9971a933d
pocket0yuka <yuka310u00@gmail.com>
2024-02-22 20:07:12 +0900

    feat:アニメーション付きフラッシュメッセージ追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,6 +20,7 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
+
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
@@ -33,6 +34,12 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
+
+  #redisを使う場合の設定
+  #config.cache_store = :redis_cache_store, {
+  #url: ENV['REDIS_URL'], # Redisの接続情報を環境変数から取得する
+  #expires_in: 1.day,    # キャッシュの有効期限を設定
+  #}
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
