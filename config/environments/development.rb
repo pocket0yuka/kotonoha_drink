4ca2aff80f09dc4192cb5630854a7f58af91d9ae
pocket0yuka <yuka310u00@gmail.com>
2024-02-22 22:40:01 +0900

    fix:fly.ioでredisに接続されないエラーを修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,7 +20,6 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
@@ -30,16 +29,10 @@ Rails.application.configure do
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
-    config.action_controller.perform_caching = true
+    config.action_controller.perform_caching = false
 
     config.cache_store = :null_store
   end
-
-  #redisを使う場合の設定
-  #config.cache_store = :redis_cache_store, {
-  #url: ENV['REDIS_URL'], # Redisの接続情報を環境変数から取得する
-  #expires_in: 1.day,    # キャッシュの有効期限を設定
-  #}
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
